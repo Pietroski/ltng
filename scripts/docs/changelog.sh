74677f91daf059f441d7ce804a53d9699253c82f
Pietroski <AugustoPTSantos@gmail.com>
2023-11-02 14:03:32 -0300

    chore: docker version bump

--- scripts/docs/changelog.sh
+++ scripts/docs/changelog.sh
@@ -6,7 +6,16 @@ cat <<EOF >CHANGELOG.md
 # [UNRELEASED]
 EOF
 
-git --no-pager log --pretty=format:"%C(auto)%d - %s" | sed 's|(tag: \(.*\))|\n# [\1]\n|' | sed 's|(\(.*\))|- [\1]|' | sed '/chore: changelog/d' | sed '/chore: version bump/d' >>CHANGELOG.md
+git --no-pager log --pretty=format:"%C(auto)%d - %s" |
+	sed 's|(tag: \(.*\))|\n# [\1]\n|' |
+	sed 's|(\(.*\))|- [\1]|' |
+	sed '/chore: changelog/d' |
+	sed '/chore: version bump/d' |
+	sed 's/gitea\/main//' |
+	sed 's/gitea\/HEAD//' |
+	sed 's/, , \]/\]/' |
+	sed 's/, \]/\]/' \
+		>>CHANGELOG.md
 
 cat <<EOF >>CHANGELOG.md
 
